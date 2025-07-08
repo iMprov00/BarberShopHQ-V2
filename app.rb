5a90b0a5358a98490a767e9bad9950cc3e137c1c
iMprov <improv00@mail.ru>
2025-07-08 18:11:04 +0700

    Передаем данные из формы визит в переменные

--- app.rb
+++ app.rb
@@ -25,3 +25,13 @@ get '/visit' do
 
 end
 
+post '/visit' do  
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+end
+
